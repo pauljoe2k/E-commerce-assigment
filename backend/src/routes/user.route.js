7b8f4d7bee0af87d861a697bb67b20b17031b848
PAUL <paulkoshy890@gmail.com>
2025-01-05 17:40:45 +0530

    milestone7

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -3,11 +3,16 @@ const upload = require('../middlewares/multer.js');
 const {
   CreateUSer,
   verifyUserController,
+  signup,
+  login,
 } = require('../controllers/user.controller.js');
 const jwt = require('jsonwebtoken');
 const router = express.Router();
 
 router.post('/create-user', upload.single('file'), CreateUSer);
 router.get('/activation/:token', verifyUserController);
+
+router.post('/signup', signup);
+router.post('/login', login);
 
 module.exports = router;
