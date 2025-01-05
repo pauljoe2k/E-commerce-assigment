50ae1dcfaad5017f7d7c949c8c3a7bfa9afbd2cf
PAUL <paulkoshy890@gmail.com>
2025-01-05 17:47:29 +0530

    milestone8

--- frontend/React/src/App.jsx
+++ frontend/React/src/App.jsx
@@ -1,15 +1,16 @@
-import LoginPage from './component/auth/Login';
-import SignupForm from './component/auth/Signup';
 import { Routes, Route } from 'react-router-dom';
+import HomePage from './Pages/HomePage';
+import Signup from './Pages/Signup';
+import Login from './Pages/Login';
 function App() {
   return (
     <>
       {/* <LoginPage /> */}
       {/* <SignupForm /> */}
       <Routes>
-        <Route path="/" element={<h1>Home Page</h1>} />
-        <Route path="/signup" element={<SignupForm />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </>
   );
