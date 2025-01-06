efe1b1f60d40df026c027714ba9b60eb858bc759
PAUL <paulkoshy890@gmail.com>
2025-01-06 07:57:34 +0530

    milestone9

--- frontend/React/src/pages/homepage.jsx
+++ frontend/React/src/pages/homepage.jsx
@@ -9,20 +9,20 @@ function HomePage() {
   console.log(data);
 
   return (
-    <>
-      <h1 className="text-center">'Home Page fro Follow along';</h1>
+    <div>
+      <h1 className="text-center">'Home Page fro Follow along'</h1>
 
       <div className="grid grid-cols-3">
         {data.map((ele, index) => {
           return (
-            <div style={{ margin: 'auto' }}>
-              <Card title={ele.title} Index={index} />;
+            <div style={{ margin: 'auto' }} className="border border-white">
+              <Card title={ele.title} Index={index} />
             </div>
           );
         })}
       </div>
-    </>
+    </div>
   );
 }
 
-export default HomePage;
+export default HomePage; 
