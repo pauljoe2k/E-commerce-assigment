5406842e229d4ce5badc4e681f62b506a26414b8
Paul <paulkoshy890@gmail.com>
2025-01-29 23:33:54 +0530

    milestone_19

--- backend/src/utils/cloudinary.js
+++ backend/src/utils/cloudinary.js
@@ -6,9 +6,10 @@ if (process.env.NODE_ENV !== 'PRODUCTION') {
     path: '../config/.env',
   });
 }
-
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.API_KEY,
   api_secret: process.env.API_SECRET,
 });
+
+module.exports = cloudinary;
--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -1,25 +1,20 @@
 import axios from 'axios';
 import { useState } from 'react';
-
-import { Link } from 'react-router-dom';
-
-
+import { Link, useNavigate } from 'react-router-dom';
 function LoginPage() {
-  const [credentials, setCredentials] = useState({
+  const [credentials, setCreds] = useState({
     email: '',
     password: '',
   });
-
   const navigate = useNavigate();
-
   const handleChange = (event) => {
     const { name, value } = event.target;
-    setCredentials({
+    console.log(name, value);
+    setCreds({
       ...credentials,
       [name]: value,
     });
   };
-
   const handleClickLogin = async (e) => {
     // axios request to backend
     e.preventDefault();
@@ -28,60 +23,72 @@ function LoginPage() {
       credentials
     );
     localStorage.setItem('token', response.data.token);
-    console.log(data);
+    console.log(response.data);
     navigate('/');
-
   };
 
   return (
-    <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+    <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+      <div class="sm:mx-auto sm:w-full sm:max-w-sm">
         <img
-          className="mx-auto h-10 w-auto"
+          class="mx-auto h-10 w-auto"
           src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
           alt="Your Company"
         />
-        <h2 className="mt-10 text-center text-2xl font-bold tracking-tight text-gray-900">
-          Login to your account
+        <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
+          Login in to your account
         </h2>
       </div>
 
-
       <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
         <form class="space-y-6" onSubmit={handleClickLogin}>
-
           <div>
-            <label htmlFor="email" className="block text-sm font-medium text-gray-900">
+            <label
+              for="email"
+              class="block text-sm/6 font-medium text-gray-900"
+            >
               Email address
             </label>
-            <div className="mt-2">
+            <div class="mt-2">
               <input
                 type="email"
                 name="email"
                 id="email"
-                autoComplete="email"
+                autocomplete="email"
                 required
                 value={credentials.email}
                 onChange={handleChange}
-                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:outline-indigo-600"
+                class=" text-black block w-full rounded-md bg-white px-3 py-1.5 text-base outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
               />
             </div>
           </div>
 
           <div>
-            <label htmlFor="password" className="block text-sm font-medium text-gray-900">
-              Password
-            </label>
-            <div className="mt-2">
+            <div class="flex items-center justify-between">
+              <label
+                for="password"
+                class="block text-sm/6 font-medium text-gray-900"
+              >
+                Password
+              </label>
+              <div class="text-sm">
+                <a
+                  href="#"
+                  class="font-semibold text-indigo-600 hover:text-indigo-500"
+                >
+                  Forgot password?
+                </a>
+              </div>
+            </div>
+            <div class="mt-2">
               <input
                 type="password"
                 name="password"
                 id="password"
-                autoComplete="current-password"
+                autocomplete="current-password"
                 required
-                value={credentials.password}
                 onChange={handleChange}
-                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:outline-indigo-600"
+                class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
               />
             </div>
           </div>
@@ -89,14 +96,13 @@ function LoginPage() {
           <div>
             <button
               type="submit"
-              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               Sign in
             </button>
           </div>
-          {error && <p className="text-red-500 text-center">{error}</p>}
           <p className="text-center">
-            Don't have an account? <Link to="/signup" className="text-indigo-600 hover:text-indigo-500">Sign up</Link>
+            Don't have an account ? <Link to={'/signup'}>Sign up</Link>
           </p>
         </form>
       </div>
